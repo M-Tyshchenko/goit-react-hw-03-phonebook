6d0c3590928d06554c713ef930d528719b2ee5a1
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-08-17 09:40:13 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,36 @@ import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 
+const localStorageKey = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  addContact = newContact => {
-    this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase()) ===
-    undefined
+  componentDidMount() {
+    const storageContacts = localStorage.getItem(localStorageKey);
+    if (storageContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(storageContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+      if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        localStorageKey,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+
+    addContact = newContact => {
+    this.state.contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    ) === undefined
       ? this.setState(prevState => {
           return { contacts: [...prevState.contacts, newContact] };
         })
